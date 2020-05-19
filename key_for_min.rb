e924152bb7b005023cbe8e543d356ae76d8b2d95
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-19 19:19:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def smallest_hash_value(hash)
+  new_hash = {}
   lowest_value = 100
+  
   hash.each do {|key, value|}
   if value < lowest_value
     lowest_value = value
     lowest_key = key
   end
+  new_hash << lowest_key
 end
-  lowest_key
+  new_hash
 end
